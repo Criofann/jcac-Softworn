159e620bd51d2b1f81bf34f6eff05aa6f29618d6
Autumn Craig <autumncraig@c02fl0g0ml85.client.kainos.com>
2023-09-21 11:53:50 +0100

    Changing the indentation to make it look better

--- clienttable.sql
+++ clienttable.sql
@@ -2,7 +2,7 @@ USE JCAC_CormacF;
 DELIMITER $$
 DROP PROCEDURE IF EXISTS my_client_table $$
 CREATE PROCEDURE my_client_table()
-BEGIN 
+BEGIN
     START TRANSACTION;
 
       CREATE TABLE Client (
@@ -13,21 +13,21 @@ BEGIN
       );
 
       ALTER TABLE Client
-       ADD PRIMARY KEY(ClientID);
+      ADD PRIMARY KEY(ClientID);
 
-ALTER TABLE Client MODIFY ClientID int AUTO_INCREMENT;
+      ALTER TABLE Client MODIFY ClientID int AUTO_INCREMENT;
 
     -- Insert a new record into the "Client" table
-    INSERT INTO Client (Name,Address,Phone)
-        VALUES ('Shaun Gauley','4-6 Upper Crescent, Belfast','028 9057 1100'), ('Denise McShaw','Lisburn Road','028 7748 4523'), ('Flora Sava','Carneybaun Estate','028 9073 2278'),
-        ('Peter Blair','York Street','028 0094 3853'), ('Mark Landing','Ballybogey','028 8746 32983'), ('Fiona Carey','Smithfield Lane','028 8535 3254'), 
-        ('Theresa August','Willowbrook Park','028 7486 4320'), ('Mary Gilbert','Princess Street','028 7473 9347'), ('Matthew Smith','Corbyfield Park','028 0274 3439'),
-        ('William McDonald','Sherbert Lane','028 4376 8203');
+      INSERT INTO Client (Name,Address,Phone)
+      VALUES ('Shaun Gauley','4-6 Upper Crescent, Belfast','028 9057 1100'), ('Denise McShaw','Lisburn Road','028 7748 4523'), ('Flora Sava','Carneybaun Estate','028 9073 2278'),
+      ('Peter Blair','York Street','028 0094 3853'), ('Mark Landing','Ballybogey','028 8746 32983'), ('Fiona Carey','Smithfield Lane','028 8535 3254'),
+      ('Theresa August','Willowbrook Park','028 7486 4320'), ('Mary Gilbert','Princess Street','028 7473 9347'), ('Matthew Smith','Corbyfield Park','028 0274 3439'),
+      ('William McDonald','Sherbert Lane','028 4376 8203');
 
 
-    -- Check the number of affected rows 
+    -- Check the number of affected rows
     GET DIAGNOSTICS @rows = ROW_COUNT;
-    IF @rows = 0 THEN 
+    IF @rows = 0 THEN
        -- If an error occurred, rollback the transaction
        ROLLBACK;
        SELECT 'Transaction rolled back due to error.';
