250f1d19f98fbac80b8c9b58a7ce3eb7efb32ed8
Criofan McCarney-Savage <criofan.mccarneysavage@kainos.com>
2023-09-21 10:44:47 +0100

    Making some edits to the create project table file to resolve some formatting issues

--- ProjectScript_4.sql
+++ ProjectScript_4.sql
@@ -1,3 +1,4 @@
+USE JCAC_CormacF;
 DELIMITER $$
 DROP PROCEDURE IF EXISTS my_project_table $$
 CREATE PROCEDURE my_project_table()
@@ -9,14 +10,22 @@ BEGIN
 	ProjectID INT PRIMARY KEY AUTO_INCREMENT,
 	ProjectName VARCHAR(70),
     `Value` DECIMAL (10,2),
-	Technology VARCHAR (255));
+	Technology VARCHAR (255)
+    TechLead 
+    );
     
     # Inserting values into the project table
 	INSERT INTO Project (ProjectName, `Value`, Technology) VALUES
-	("Project1", 100.27, "SQL, Java, Python"), ("Project2", 1000, "SQL, Java, RubyOnRails"),
-    ("Project3", 10000, "Java, HTML, CSS"), ("Project4", 250000, "AppInventor, Scratch, Python"),
-    ("Project5", 19233.91, "Visual Basic"), ("Project6", 0.81, "Binary"), ("Project7", 999999.99, "SQl, Java, Visual Basic, Scratch"),
-    ("Project8", 0.01, "Binary"), ("Project9", 13, "C#, C++"), ("Project10", 10000000.01, "Machine Code"); 
+	("Project1", 100.27, "SQL, Java, Python"),
+    ("Project2", 1000, "SQL, Java, RubyOnRails"),
+    ("Project3", 10000, "Java, HTML, CSS"),
+    ("Project4", 250000, "AppInventor, Scratch, Python"),
+    ("Project5", 19233.91, "Visual Basic"), 
+    ("Project6", 0.81, "Binary"),
+    ("Project7", 999999.99, "SQl, Java, Visual Basic, Scratch"),
+    ("Project8", 0.01, "Binary"),
+    ("Project9", 13, "C#, C++"),
+    ("Project10", 10000000.01, "Machine Code"); 
     
     SELECT * FROM Project;
 
